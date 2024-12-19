6af0786a5a3001f905e18a79a8b0cf6f6262dfa3
RichardPadilla7 <richardpadilla3rod@gmail.com>
2024-12-19 09:27:07 -0500

    RICHARD

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
 public class Principal {
+    int[] cedula = new int[3];
+    String[] nombre = new String[3];
+    int totalIngresos = 0;
+
+    public void agregarClientes(int cedula,String nombre){
+
+        if(totalIngresos < 3){
+
+            this.cedula[totalIngresos] = cedula;
+            this.nombre[totalIngresos] = nombre;
+            totalIngresos++;
+            System.out.println("CLIENTE AGREGADO!");
+        } else {
+            System.out.println("NO SE PUEDE AGREGAR MAS CLIENTES!");
+        }
+    }
+
+
+    public void mostrarInformacion(){
+
+        if(totalIngresos == 0){
+            System.out.println("NO HAY CLIENTES INGRESADOS!");
+        } else {
+            System.out.println("Informacion de clientes:");
+            for(int i=0; i<totalIngresos; i++){
+                System.out.println("Cedula: " + cedula[i]);
+                System.out.println("Nombre: " + nombre[i]);
+            }
+        }
+    }
 }
